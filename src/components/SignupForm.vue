4f5ba048fd1b5eceec99de4e506ca543a61b5a3a
TDL2627 <the.dark.lord.2627@gmail.com>
2022-01-23 01:00:48 +0200

    update

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,90 @@
+<template>
+  <form> 
+<label>Email</label>
+<input type="email" v-model="email">
+
+<label>Password:</label>
+<input type="password" v-model="password">
+
+<label>Role:</label>
+<select v-model="role">
+  <option value="Mentor">Mentor</option>
+  <option value="Mentee">Mentee</option>
+
+</select>
+
+<div class="terms">
+ <input type="checkbox" v-model="terms" required>
+ <label >Accept Terms and Conditions
+ </label>
+</div>
+
+<input type="checkbox" value="Dax" v-model="names">
+<label >Dax</label>
+<input type="checkbox" value="Palesa" v-model="names">
+<label >Palesa</label>
+<input type="checkbox" value="Ghost" v-model="names">
+<label >Ghost</label>
+  </form>
+
+
+  <p>Email: {{email}}</p>
+  <p>Password: {{password}}</p>
+  <p>Role: {{role}}</p>
+  <p>Terms and Conditions : {{terms}}</p>
+  <p>Names:{{names}}</p>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      email:"",
+      password:"",
+      role:"",
+      term:false,
+      names:[]
+    }
+  }
+
+}
+
+</script>
+
+<style>
+form {
+    max-width: 420px;
+    margin: 30px auto;
+   
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;
+}
+label {
+    color: #aaa;
+    display: inline-block;
+    margin: 25px 0 15px;
+    font-size: 0.6em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
+}
+input {
+    display: block;
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: #555;
+}
+input[type="checkbox"] {
+    display: inline-block;
+    width: 16px;
+    margin: 0 10px 0 0;
+    position: relative;
+    top: 2px;
+}
+
+
+</style>
